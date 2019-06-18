810c9ed3623bf1e3583548d26b17caef7866ed1d
dremsis <JostQ>
2019-06-18 09:42:06 +0200

    webpack

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -63,12 +63,13 @@ class ArticleController extends AbstractController
     /**
      * @Route("/{id}/edit", name="article_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, Article $article): Response
+    public function edit(Request $request, Article $article, Slugify $slugify): Response
     {
         $form = $this->createForm(ArticleType::class, $article);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $article->setSlug($slugify->generate($article->getTitle()));
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('article_index', [
--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Tag;
+use App\Form\TagType;
 use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class TagController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="index", methods={"GET"})
      */
     public function index(TagRepository $tagRepository) : Response
     {
@@ -24,7 +26,26 @@ class TagController extends AbstractController
     }
 
     /**
-     * @Route("/{name}", name="show")
+     * @Route("/new", name="new", methods={"GET", "POST"})
+     */
+    public function new(Request $request) : Response
+    {
+        $form = $this->createForm(TagType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $tag = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($tag);
+            $em->flush();
+            return $this->redirectToRoute('tag_show', ['name' => $tag->getName()]);
+        }
+
+        return $this->render('tag/new.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/{name}", name="show", methods={"GET"})
      */
     public function show(Tag $tag) : Response
     {
@@ -33,5 +54,18 @@ class TagController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/{name}", name="delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Tag $tag) : Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$tag->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($tag);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('tag_index');
+    }
 
 }
